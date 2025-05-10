0e5a223b296cb41a0b2b480286832457dc80c4d3
mrizkyprtm <m.rizkypratama319@gmail.com>
2025-05-11 00:34:44 +0700

    create search, filter and sort feature on posts page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,12 +3,32 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Models\User;
+use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-  public function __invoke()
+  public function __invoke(Request $request)
   {
-    $posts = Post::with('user')->latest()->paginate(6);
-    return view('front.home', compact('posts'));
+    $query = Post::with('user');
+
+    if ($request->filled('search')) {
+      $query->where('title', 'like', '%' . $request->search . '%');
+    }
+
+    if ($request->filled('author')) {
+      $query->where('user_id', $request->author);
+    }
+
+    if ($request->filled('order') && in_array($request->order, ['asc', 'desc'])) {
+      $query->orderBy('created_at', $request->order);
+    } else {
+      $query->latest();
+    }
+
+    $posts = $query->latest()->paginate(10)->withQueryString();
+    $authors = User::orderBy('name')->get();
+    // $posts = Post::with('user')->latest()->get();
+    return view('front.home', compact('posts', 'authors'));
   }
 }
