f290d4b1e22f5e85bd384151e8a285829871f999
mrizkyprtm <m.rizkypratama319@gmail.com>
2025-05-10 16:57:43 +0700

    add fontawesome cdn

--- resources/views/front/layouts/main.blade.php
+++ resources/views/front/layouts/main.blade.php
@@ -13,10 +13,13 @@
   <link href="https://fonts.bunny.net" rel="preconnect">
   <link href="https://fonts.bunny.net/css?family=inter:400,500,600,700" rel="stylesheet" />
 
+  {{-- FontAwesome Icon --}}
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" rel="stylesheet">
+
   {{-- Style --}}
   @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-  <link href="{{ asset('build/assets/app-BZEpNJK_.css') }}" rel="stylesheet">
+  <link href="{{ asset('build/assets/app-7brdmFnY.css') }}" rel="stylesheet">
   <script src="{{ asset('build/assets/app-Bf4POITK.js') }}"></script>
 </head>
 
