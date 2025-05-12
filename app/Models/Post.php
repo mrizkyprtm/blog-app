6e2820ab0201908782202c7b68fd59aac1196cfc
mrizkyprtm <m.rizkypratama319@gmail.com>
2025-05-12 11:51:27 +0700

    create local scope on model for filtering data posts

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Contracts\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Attributes\Scope;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
@@ -40,5 +42,34 @@ class Post extends Model
         'source' => 'title'
       ]
     ];
+  }
+
+  #[Scope]
+  protected function searchBy(Builder $query, string $column): void
+  {
+    $query->where($column, 'like', '%' . request('search') . '%');
+  }
+
+  #[Scope]
+  public function filter(Builder $query): void
+  {
+    if ($slug = request('category')) {
+      $query->whereHas('category', fn($q) => $q->where('slug', $slug));
+    }
+
+    if ($username = request('author')) {
+      $query->whereHas('user', fn($q) => $q->where('username', $username));
+    }
+  }
+
+  #[Scope]
+  public function orderByDate($query): void
+  {
+    $order = request('order');
+    if (in_array($order, ['asc', 'desc'])) {
+      $query->orderBy('created_at', $order);
+    } else {
+      $query->latest();
+    }
   }
 }
